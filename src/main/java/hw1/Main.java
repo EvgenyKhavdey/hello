2a96a23c06afdabc7c68a8e25c931736324fb8c3
Evgeny Khavdey <khavdey61@yandex.ru>
2021-01-14 22:27:08 +0300

    Домашнее задание

---
+++ src/main/java/hw1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+//    Задание 1
+    public static void main (String [] args) {
+        System.out.println("Hello main");
+    }
+
+}
---
+++ src/main/java/hw3/Expression.java
@@ -0,0 +1,13 @@
+public class Expression {
+//    задание 1.3
+
+    public static void expression (String [] args){
+        System.out.println(result());
+    }
+
+    public static int result(float a, float b, float c, float d) {
+
+
+        return a*(b - (c/d));
+    }
+}
