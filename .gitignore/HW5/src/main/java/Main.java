6acbc174b05c538ea39f6e1f9c638725616e8365
Evgeny Khavdey <khavdey61@yandex.ru>
2021-03-08 10:22:45 +0300

    ДЗ 7

--- .gitignore/HW5/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Thread t1 = new Thread(new TestThread());
-        Thread t2 = new Thread(new TestThreadMethod2());
-        t1.start();
-        t2.start();
-    }
-}
--- .gitignore/src/main/java/hw7/Main.java
+++
@@ -1,9 +0,0 @@
-package hw7;
-
-public class Main {
-
-    public static void main (String[] args){
-        String name = "Евгений";
-        System.out.println("Привет, " + name);
-    }
-}
